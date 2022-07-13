582d10d5dcfb434cb61c6ee0eafd565cca2f3f96
otyamura <suzuki08suzuki@gmail.com>
2022-07-14 01:29:38 +0900

    change port

--- cmd/vulneb-go/main.go
+++ cmd/vulneb-go/main.go
@@ -74,6 +74,63 @@ func main() {
 		})
 	})
 
+	r.GET("/login/4", func(c *gin.Context) {
+		c.HTML(200, "login.html", gin.H{
+			"error": "",
+		})
+	})
+
+	r.POST("/login/4", func(c *gin.Context) {
+		id := c.PostForm("id")
+		pass := c.PostForm("password")
+		fmt.Println(id, pass)
+		query := "SELECT * FROM accounts WHERE id=? AND password=?"
+		stmt, err := db.Prepare(query)
+		if err != nil {
+			c.HTML(200, "login.html", gin.H{
+				"error": err,
+				"query": query,
+			})
+			return
+		}
+		rows, err := stmt.Query(id, pass)
+		if err != nil {
+			c.HTML(200, "login.html", gin.H{
+				"error": err,
+				"query": query,
+			})
+			return
+		}
+		defer rows.Close()
+
+		var accounts []Account
+		if !rows.Next() {
+			c.HTML(200, "login.html", gin.H{
+				"error": "no rows in result set",
+				"query": query,
+			})
+			return
+		} else {
+			var a Account
+			err := rows.Scan(&a.ID, &a.Password)
+			if err != nil {
+				log.Fatal(err)
+			}
+			accounts = append(accounts, a)
+		}
+		for rows.Next() {
+			var a Account
+			err := rows.Scan(&a.ID, &a.Password)
+			if err != nil {
+				log.Fatal(err)
+			}
+			accounts = append(accounts, a)
+		}
+		c.HTML(200, "success.html", gin.H{
+			"accounts": accounts,
+		})
+	})
+
 	r.POST("/login/3", func(c *gin.Context) {
 		id := c.PostForm("id")
 		pass := c.PostForm("password")
@@ -114,22 +171,6 @@ func main() {
 		}
 		c.HTML(200, "success.html", gin.H{
 			"accounts": accounts,
-		})
-	})
-
-	r.GET("/tldr", func(c *gin.Context) {
-		c.HTML(200, "tldr.html", nil)
-	})
-	r.POST("/tldr", func(c *gin.Context) {
-		name := c.PostForm("name")
-		cmd := "tldr " + name
-		remove_color := `'| sed -e 's/\x1b\[[0-9;]*m//g' 1>&2`
-		result, _ := exec.Command("sh", "-c", cmd).CombinedOutput()
-		removed, _ := exec.Command("sh", "-c", "echo '"+string(result)+remove_color).CombinedOutput()
-
-		c.HTML(200, "tldr.html", gin.H{
-			"command": cmd,
-			"result":  string(removed),
 		})
 	})
 
